0c55fa5d8fc4da9959e7dfc12ea9aba04c120fe2
Valium Caris <mncaris@cyberdom.cl>
2017-05-25 15:52:38 -0400

    primera subida de archivos simce2017

---
+++ sistema/app/views/modules/login-registro/lr.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-index',
+  templateUrl: './lr.component.html',
+  styleUrls: ['./lr.component.css']
+})
+export class AppCom {
+  title = 'Proyecto simce 2017';
+}
